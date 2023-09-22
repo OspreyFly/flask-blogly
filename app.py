8b83b58714341ac810519d77d578c5a60849065f
OspreyFly <noahbengoff@gmail.com>
2023-09-22 15:10:09 -0500

    Added HTML templates and more views

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-"""Blogly application."""
-
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, request, redirect
+from models import db, connect_db, Person
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secretKEY"
 
 connect_db(app)
-db.create_all()
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+@app.route("/user_listing")
+def userDir():
+    users = Person.query.all()
+    return render_template("users.html", users=users)
+
+@app.route("/add_person")
+def showForm():
+    return render_template("user_new_form.html")
+
+@app.route("/add_person", methods=["POST"])
+def addPerson():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    img_url = request.form['img_url']
+
+    person = Person(first_name, last_name, img_url)
+    
+    db.session.add(person)
+    db.session.commit()
+
+    return redirect(f"/{person.id}")
+    
+
+@app.route("/<int:person_id>")
+def show_person(person_id):
+    person = Person.query.get_or_404(person_id)
+    return render_template("user_detail_page.html", person=person)
+
+
--- models.py
+++ models.py
@@ -1,1 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app=app
+    db.init_app(app)
+
 """Models for Blogly."""
+class Person(db.Model):
+    __tablename__ = "people"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    img_url = db.Column(db.String(50))
+    
+#default="https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png"
